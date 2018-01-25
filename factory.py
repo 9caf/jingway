cedad66c3f32f081187ef8982a91cb1c4506466e
9caf <106035405@qq.com>
2018-01-26 02:36:58 +0800

    使用python命令创建数据库

--- factory.py
+++ factory.py
@@ -2,49 +2,43 @@ from flask import Flask, g
 from werkzeug.utils import find_modules, import_string
 from blueprints.users import bp
 
+
 def create_app(config=None):
+    app = Flask('jingway')
+    app.config.from_pyfile('config.cfg')
+    init_db(app)
+    register_blueprints(app)
+    return app
+
+
+def init_db(app):
+    import sys
+    sys.path.append(app.root_path) 
+    from models import db
+    db.init_app(app)
+
+
+def create_db():
     app = Flask('jingway')
     app.config.from_pyfile('config.cfg')
 
     import sys
     sys.path.append(app.root_path) 
     from models import db
-
     db.init_app(app)
 
-    register_blueprints(app)
-    register_cli(app)
-    register_teardowns(app)
-
-    return app
-
+    db.create_all(app=app)
+    print('数据库创建成功！')
+   
+    
 
 def register_blueprints(app):
-    """Register all blueprint modules
-
-    Reference: Armin Ronacher, "Flask for Fun and for Profit" PyBay 2016.
-    """
+    """蓝图注册"""
     for name in find_modules('blueprints'):
         mod = import_string(name)
         if hasattr(mod, 'bp'):
             app.register_blueprint(mod.bp)
     return None
-
-
-def register_cli(app):
-    @app.cli.command('initdb')
-    def initdb_command():
-        """Creates the database tables."""
-        db.create_all(app=create_app())
-        print('Initialized the database.')
-
-
-def register_teardowns(app):
-    @app.teardown_appcontext
-    def close_db(error):
-        """Closes the database again at the end of the request."""
-        if hasattr(g, 'sqlite_db'):
-            g.sqlite_db.close()
 
 
 if __name__ == '__main__':
